37429e9a968108c877351f3ab2e1b7b6b5506f56
mayoreee <mayoreee1@gmail.com>
2024-09-29 14:11:27 +0200

    chore: use setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dapiclient",
+    version="0.0.7",
+    description="A Python client SDK for DASH Platform",
+    long_description=open('README.md').read(),  # Assuming you have a README.md
+    long_description_content_type="text/markdown",
+    author="mayoreee",
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=[
+        "base58>=2.1.0",
+        "cbor2>=5.4.0",
+        "certifi>=2021.5.30",
+        "charset-normalizer>=2.0.3",
+        "grpcio>=1.38.1",
+        "idna>=3.2",
+        "protobuf>=3.17.3",
+        "requests>=2.26.0",
+        "six>=1.16.0",
+        "urllib3>=1.26.6",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.9',
+    url="https://github.com/mayoreee/dapi-client-py",
+    project_urls={
+        "Homepage": "https://github.com/mayoreee/dapi-client-py",
+        "Repository": "https://github.com/mayoreee/dapi-client-py",
+    },
+)
